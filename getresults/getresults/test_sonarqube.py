fc205b8b4631fec907a98c269ca6e9212c19e7fd
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2016-06-17 16:25:37 -0700

    Trying to get mock with multiple return values to work

--- getresults/getresults/test_sonarqube.py
+++ getresults/getresults/test_sonarqube.py
@@ -34,46 +34,46 @@ class TestGetSonarQubeData:
     def test_requests_1_page(self, mock_requests):
         """
         Given Mock requests configured to return a data set with page info indicating one page
-        When Call get_sonarqube_data with a valid URL
+        When Call query_sonarqube_data with a valid URL
         Then requests.get is called until the page number reaches the expected end.
         """
-        json_one_page = copy.deepcopy(self.sonarqube_sample)
-        json_one_page['ps'] = 100
-        json_one_page['total'] = 100
+        json_page = copy.deepcopy(self.sonarqube_sample)
+        json_page['ps'] = 100
+        json_page['total'] = 100
         mock_requests.configure_mock(
                 **{'get.return_value': mock.MagicMock(
-                        **{'text': json.dumps(json_one_page)})})
-        sonarqube.get_sonarqube_data('url')
+                        **{'text': json.dumps(json_page)})})
+        sonarqube.query_sonarqube_data('url')
         mock_requests.get.assert_called_once_with(url='url')
 
     def test_requests_1_item(self, mock_requests):
         """
         Given Mock requests configured to return a data set with page info indicating a single entry
-        When Call get_sonarqube_data with a valid URL
+        When Call query_sonarqube_data with a valid URL
         Then requests.get is called until the page number reaches the expected end.
         """
-        json_one_page = copy.deepcopy(self.sonarqube_sample)
-        json_one_page['ps'] = 100
-        json_one_page['total'] = 1
+        json_page = copy.deepcopy(self.sonarqube_sample)
+        json_page['ps'] = 100
+        json_page['total'] = 1
         mock_requests.configure_mock(
                 **{'get.return_value': mock.MagicMock(
-                        **{'text': json.dumps(json_one_page)})})
-        sonarqube.get_sonarqube_data('url')
+                        **{'text': json.dumps(json_page)})})
+        sonarqube.query_sonarqube_data('url')
         mock_requests.get.assert_called_once_with(url='url')
 
     def test_requests_1_page_plus_1(self, mock_requests):
         """
         Given Mock requests configured to return a data set with paging info indicating one page plus one more item.
-        When Call get_sonarqube_data with a valid URL
+        When Call query_sonarqube_data with a valid URL
         Then requests.get is called until the page number reaches the expected end.
         """
-        json_one_page = copy.deepcopy(self.sonarqube_sample)
-        json_one_page['ps'] = 100
-        json_one_page['total'] = 101
+        json_page = copy.deepcopy(self.sonarqube_sample)
+        json_page['ps'] = 100
+        json_page['total'] = 101
         mock_requests.configure_mock(
                 **{'get.return_value': mock.MagicMock(
-                        **{'text': json.dumps(json_one_page)})})
-        sonarqube.get_sonarqube_data('url')
+                        **{'text': json.dumps(json_page)})})
+        sonarqube.query_sonarqube_data('url')
         mock_requests.get.assert_has_calls([mock.call(url='url'),
                                             mock.call(url='url', data={'p': 2})])
 
@@ -81,16 +81,16 @@ class TestGetSonarQubeData:
         """
         Given Mock requests configured to return a data set with page info indicating two full pages, then return
             the second page
-        When Call get_sonarqube_data with a valid URL
+        When Call query_sonarqube_data with a valid URL
         Then requests.get is called until the page number reaches the expected end.
         """
-        json_one_page = copy.deepcopy(self.sonarqube_sample)
-        json_one_page['ps'] = 100
-        json_one_page['total'] = 200
+        json_page = copy.deepcopy(self.sonarqube_sample)
+        json_page['ps'] = 100
+        json_page['total'] = 200
         mock_requests.configure_mock(
                 **{'get.return_value': mock.MagicMock(
-                        **{'text': json.dumps(json_one_page)})})
-        sonarqube.get_sonarqube_data('url')
+                        **{'text': json.dumps(json_page)})})
+        sonarqube.query_sonarqube_data('url')
         mock_requests.get.assert_has_calls([mock.call(url='url'),
                                             mock.call(url='url', data={'p': 2})])
 
@@ -98,16 +98,16 @@ class TestGetSonarQubeData:
         """
         Given Mock requests configured to return a data set with page info indicating three full pages, then return
             the second and third pages
-        When Call get_sonarqube_data with a valid URL
+        When Call query_sonarqube_data with a valid URL
         Then requests.get is called until the page number reaches the expected end.
         """
-        json_one_page = copy.deepcopy(self.sonarqube_sample)
-        json_one_page['ps'] = 100
-        json_one_page['total'] = 300
+        json_page = copy.deepcopy(self.sonarqube_sample)
+        json_page['ps'] = 100
+        json_page['total'] = 300
         mock_requests.configure_mock(
                 **{'get.return_value': mock.MagicMock(
-                        **{'text': json.dumps(json_one_page)})})
-        sonarqube.get_sonarqube_data('url')
+                        **{'text': json.dumps(json_page)})})
+        sonarqube.query_sonarqube_data('url')
         mock_requests.get.assert_has_calls([mock.call(url='url'),
                                             mock.call(url='url', data={'p': 2}),
                                             mock.call(url='url', data={'p': 3})])
@@ -116,15 +116,40 @@ class TestGetSonarQubeData:
         """
         Given Mock requests configured to return a data set with page info indicating three full pages, then return
             the second and third pages
-        When Call get_sonarqube_data with a valid URL
+        When Call query_sonarqube_data with a valid URL
         Then requests.get is called until the page number reaches the expected end.
         """
-        json_one_page = copy.deepcopy(self.sonarqube_sample)
-        json_one_page['ps'] = 100
-        json_one_page['total'] = 1001
+        json_page = copy.deepcopy(self.sonarqube_sample)
+        json_page['ps'] = 100
+        json_page['total'] = 1001
         mock_requests.configure_mock(
-                **{'get.return_value': mock.MagicMock(
-                        **{'text': json.dumps(json_one_page)})})
-        sonarqube.get_sonarqube_data('url')
+                **{'get.side_effect': lambda url: iter([mock.MagicMock(
+                        **{'text': json.dumps(json_page)})])})
+        sonarqube.query_sonarqube_data('url')
         mock_requests.get.assert_has_calls(
             [mock.call(url='url')] + [mock.call(url='url', data={'p': i}) for i in range(2, 11)])
+
+    # def test_data_merging(self, mock_requests):
+    #     """
+    #     Given Mock requests configured to return a data set with page info indicating three full pages, then return
+    #         the second and third data sets
+    #     When Call query_sonarqube_data with a valid URL
+    #     Then Method returns the merged data sets.
+    #     """
+    #     json_page = copy.deepcopy(self.sonarqube_sample)
+    #     json_page['ps'] = 100
+    #     json_page['total'] = 300
+    #     json_page['components'] = ["component1"]
+    #     json_page['issues'] = ["issue1"]
+    #     json_page_two = copy.deepcopy(json_page)
+    #     json_page_two['components'] = ["component2", "component3"]
+    #     json_page_two['issues'] = ["issue2", "issue3"]
+    #     json_page_two = copy.deepcopy(json_page)
+    #     json_page_two['components'] = ["component4", "component5", "components6"]
+    #     json_page_two['issues'] = ["issue4", "issue5", "issue6"]
+    #     mock_requests.configure_mock(
+    #             **{'get.return_value': mock.MagicMock(
+    #                     **{'text': json.dumps(json_page)})})
+    #     sonarqube.query_sonarqube_data('url')
+    #     mock_requests.get.assert_has_calls(
+    #         [mock.call(url='url')] + [mock.call(url='url', data={'p': i}) for i in range(2, 11)])
